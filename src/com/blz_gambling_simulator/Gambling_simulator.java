56d5705c32193da0d2283d53811dff0514bace7f
LDeepakReddy <l.deepakreddy.com>
2022-03-03 05:23:22 +0530

    ADDED:feature to check win or lost

--- src/com/blz_gambling_simulator/Gambling_simulator.java
+++ src/com/blz_gambling_simulator/Gambling_simulator.java
@@ -6,6 +6,15 @@ public class Gambling_simulator {
 
     public static void main(String[] args) {
         System.out.println("Initial day stake is " + DAY_STAKE + " And Bet per game is " + BET_PER_GAME);
+        int totalCash = DAY_STAKE;
+        int game = (int) ((Math.random() * 10) % 2);
+        if (game == 1) {
+            totalCash += BET_PER_GAME;
+            System.out.println("Gambler won the bet : " + totalCash);
+        } else {
+            totalCash -= BET_PER_GAME;
+            System.out.println("Gambler Lost the bet : " + totalCash);
+        }
 
     }
 }
