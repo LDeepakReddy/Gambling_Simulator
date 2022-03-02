f8e2f3ff00629f1ea0a40e8d2dbec48e877e6515
LDeepakReddy <l.deepakreddy.com>
2022-03-03 05:08:00 +0530

    ADDED-Initial Day stake and Bet per day

--- src/com/blz_gambling_simulator/Gambling_simulator.java
+++ src/com/blz_gambling_simulator/Gambling_simulator.java
@@ -1,7 +1,11 @@
 package com.blz_gambling_simulator;
 
 public class Gambling_simulator {
+    public static final int DAY_STAKE = 100;
+    public static final int BET_PER_GAME = 1;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Gambling program");
+        System.out.println("Initial day stake is " + DAY_STAKE + " And Bet per game is " + BET_PER_GAME);
+
     }
 }
