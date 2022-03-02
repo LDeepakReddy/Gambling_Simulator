105fe28bb9022c61502533b03da0a243162a6f60
LDeepakReddy <l.deepakreddy.com>
2022-03-03 04:56:42 +0530

    ADDED: welcome message for gambling program

---
+++ src/com/blz_gambling_simulator/Gambling_simulator.java
@@ -0,0 +1,7 @@
+package com.blz_gambling_simulator;
+
+public class Gambling_simulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling program");
+    }
+}
