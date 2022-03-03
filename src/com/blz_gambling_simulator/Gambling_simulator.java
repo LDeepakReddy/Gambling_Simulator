83b4b7d41d47c08a20e86a424abf156a2b1a29dd
LDeepakReddy <l.deepakreddy.com>
2022-03-03 05:59:11 +0530

    ADDED:Total won or lost in 20days

--- src/com/blz_gambling_simulator/Gambling_simulator.java
+++ src/com/blz_gambling_simulator/Gambling_simulator.java
@@ -6,22 +6,36 @@ public class Gambling_simulator {
     public static final int RESIGNING_MARGIN = (DAY_STAKE / 100) * 50; // 50% of the stake
     public static final int WINNING_MARGIN = DAY_STAKE + RESIGNING_MARGIN;
     public static final int LOSING_MARGIN = DAY_STAKE - RESIGNING_MARGIN;
+    public static final int START_DAY = 1;
+    public static final int MAXIMUM_DAY = 20;
+    public static final int START_DOLLARS = 0;
+    public static final int STAKES_PER_DAY = 50;
 
     public static void main(String[] args) {
-        System.out.println("Initial day stake is " + DAY_STAKE + " And Bet per game is " + BET_PER_GAME);
         int totalCash = DAY_STAKE;
-        while (totalCash < WINNING_MARGIN && totalCash > LOSING_MARGIN) {
-            int game = (int) ((Math.random() * 10) % 2);
-            if (game == 1) {
-                totalCash += BET_PER_GAME;
+        int totalDollars = START_DOLLARS;
+        int days = START_DAY;
+        for (days = START_DAY; days <= MAXIMUM_DAY; days++) {
+            while (totalCash < WINNING_MARGIN && totalCash > LOSING_MARGIN) {
+                int game = (int) ((Math.random() * 10) % 2);
+                if (game == 1) {
+                    totalCash += BET_PER_GAME;
+                } else {
+                    totalCash -= BET_PER_GAME;
+                }
+            }
+            if (totalCash == WINNING_MARGIN) {
+                totalDollars += STAKES_PER_DAY;
+                System.out.println("Gambler won " + totalCash);
             } else {
-                totalCash -= BET_PER_GAME;
+                totalDollars -= STAKES_PER_DAY;
+                System.out.println("Gambler lost " + totalCash);
             }
         }
-        if (totalCash == WINNING_MARGIN) {
-            System.out.println("Gambler won " + totalCash);
-        } else {
-            System.out.println("Gambler lost " + totalCash);
+        if (totalDollars > START_DOLLARS) {
+            System.out.println("Gambler won " +totalDollars);
+        }else {
+            System.out.println("Gambler lost " +totalDollars);
         }
     }
 }
